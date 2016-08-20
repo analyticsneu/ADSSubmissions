4d9b512c3ddfa76e850cfec4a7828dcd88b7e470
rutujamagar <magar.r@husky.neu.edu>
2016-08-20 01:56:22 -0400

    Final Folder

---
+++ ADSAssignments/Final_Project/Handwritten_Digit_Recognition/H2OHandwritingRecognition/ui.R
@@ -0,0 +1,34 @@
+require(mxnet)
+require(imager)
+require(shiny)
+require(jpeg)
+require(png)
+
+shinyUI(pageWithSidebar(
+  headerPanel(title = 'Image Classification using MXNetR',
+              windowTitle = 'Image Classification using MXNetR'),
+  
+  sidebarPanel(
+    includeCSS('boot.css'),
+    tabsetPanel(
+      id = "tabs",
+      tabPanel("Upload Image",
+               fileInput('file1', 'Upload a PNG / JPEG File:')),
+      tabPanel(
+        "Use the URL",
+        textInput("url", "Image URL:", "http://"),
+        actionButton("goButton", "Go!")
+      )
+    )
+  ),
+  
+  mainPanel(
+    h3("Image"),
+    tags$hr(),
+    imageOutput("originImage", height = "auto"),
+    tags$hr(),
+    h3("What is in it?"),
+    tags$hr(),
+    verbatimTextOutput("res")
+  )
+))
